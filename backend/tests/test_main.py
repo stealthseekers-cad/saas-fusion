138ea4d99eb7b75620f36964f46e78b7c26ea166
Ryan M Reynolds <stealthseekers@gmail.com>
2025-08-15 12:58:24 -0600

    style: Format test files with black

--- backend/tests/test_main.py
+++ backend/tests/test_main.py
@@ -44,6 +44,4 @@ def test_read_root():
     """
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {
-        "Hello": "Stealth Seekers Foresight Engine is Online"
-    }
+    assert response.json() == {"Hello": "Stealth Seekers Foresight Engine is Online"}
